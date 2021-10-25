f30873ac5d9366752046a866a05c042ffdf75a4d
Richet <shanon.richet@gmail.com>
2021-10-25 16:35:03 +0200

    log values of clock in the console

--- script.js
+++ script.js
@@ -6,14 +6,12 @@ const anchorage= new Date().toLocaleString('en-US', {
 const date = document.createElement('p');
 date.innerHTML= 'Time in Anchorage: ' + anchorage;
 timeZone.appendChild(date)
-
 const reykjavik= new Date().toLocaleString('is-IS', {
     timeZone: 'Atlantic/Reykjavik' 
    })
 const date2 = document.createElement('p');
 date2.innerHTML= 'Time in Reykjavik: ' + reykjavik;
 timeZone.appendChild(date2)
-
 const brussels= new Date().toLocaleString('be-BE', {
     timeZone: 'Europe/Brussels' 
    })
@@ -48,10 +46,9 @@ const timeIn8000Hours= new Date(add8000Hours)
 dateIn8000Hours.innerHTML= 'We are ' + now.toLocaleDateString() + ' In 8000 hours it will be ' + timeIn8000Hours.toLocaleDateString();
 timestamp.appendChild(dateIn8000Hours)
 
-// Exercice 4
+// Exercice 4 Calendrier 
 
 const footer= document.querySelector('footer')
-
 var p1= document.createElement('p')
 p1.setAttribute('class', 'jour')
 const jour= now.toDateString().slice(0,4)
@@ -72,12 +69,76 @@ p4.setAttribute('class', 'annee')
 const annee= now.toDateString().slice(10, 15)
 p4.innerHTML= annee;
 
-const article= document.createElement('article')
-article.appendChild(p1)
-article.appendChild(p2)
-article.appendChild(p3)
-article.appendChild(p4)
-footer.appendChild(article)
+const calendrier= document.createElement('article')
+calendrier.setAttribute('class', 'calendrier')
+calendrier.appendChild(p1)
+calendrier.appendChild(p2)
+calendrier.appendChild(p3)
+calendrier.appendChild(p4)
+footer.appendChild(calendrier)
+
+// Horloge 
+
+const horloge= document.createElement('article');
+horloge.setAttribute('class', 'horloge')
+var p5= document.createElement('p')
+var heures= now.toLocaleTimeString().slice(0, 2)
+var minutes= now.toLocaleTimeString().slice(2, 5)
+var secondes= now.toLocaleTimeString().slice(5, 8)
+const time= heures + minutes + secondes;
+p5.innerHTML= time;
+
+horloge.appendChild(p5)
+footer.appendChild(horloge)
+
+// Find Timestamp of a given date
+
+const main= document.querySelector('main')
+const dateReponse= document.createElement('input')
+dateReponse.value= "July 1, 1999";
+dateReponse.placeholder= 'July 1, 1999'
+
+let y = new Date(dateReponse.value)
+
+let tempsPassé= Math.floor((now.getTime()-y.getTime())/ 1000);
+const tempsEcoulé= document.createElement('p')
+tempsEcoulé.setAttribute('class', 'temps_écoulé')
+if (tempsPassé < 24 * 60 * 60){
+    tempsEcoulé.innerHTML= "Nous sommes " + now.toLocaleDateString() + ' ' + Math.floor(tempsPassé/24 /60 /60) + ' ' + "jours sont passés depuis" + ' ' + y.toLocaleDateString();
+}
+if ((tempsPassé > 24 * 60 * 60)&& (tempsPassé < 30 * 24 * 60 * 60)){
+    tempsEcoulé.innerHTML= "Nous sommes " + now.toLocaleDateString() + ' ' + Math.floor(tempsPassé/60 /60 /24 /30) + ' ' + "mois sont passés depuis" + ' ' + y.toLocaleDateString();
+}
+if (tempsPassé > 356 * 24 * 60 * 60){
+    tempsEcoulé.innerHTML= "Nous sommes " + now.toLocaleDateString() + ' ' + Math.floor(tempsPassé/365 /24 /60 /60) + ' ' + "années sont passées depuis" + ' ' + y.toLocaleDateString();
+}
+
+tableauDuTemps= [
+    {
+        name: 'semaine',
+        value: 1000 * 60 * 60 * 24 * 7
+    },
+    {
+        name: 'day',
+        value: 1000 * 60 * 60 * 24
+    },
+    {
+        name: 'hour',
+        value: 1000 * 60 * 60
+    },
+    {
+        name: 'minute', 
+        value: 1000 * 60
+    },
+    {
+        name: 'second',
+        value: 1000
+    }
+]
+main.appendChild(tempsEcoulé)
+main.appendChild(dateReponse)
+
+
 
 
 
